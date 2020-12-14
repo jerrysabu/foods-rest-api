bee22fb0ffa6491de86579d3271ef272e9723b4e
jerry sabu <studyboredjerrysabu@gmail.com>
2020-12-14 21:53:58 +0530

    runserver

--- src/foods_project/foods_api/views.py
+++ src/foods_project/foods_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework import generics, filters
+from . models import Food
+from . serializers import FoodSerializer
 # Create your views here.
+
+class FoodAPIView(generics.ListCreateAPIView):
+
+    search_fields = ['name','price']
+    filter_backends = (filters.SearchFilter,)
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
